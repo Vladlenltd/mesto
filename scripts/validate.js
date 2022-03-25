e0ff04e909c68cc2c24cc828ff47063a01452153
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-03-25 21:04:49 +0300

    feature:js/validate сделать disable кнопки для отправки данных

--- scripts/validate.js
+++ scripts/validate.js
@@ -9,36 +9,65 @@ const enableValidation =  {
     inputErrorClass: 'popup__input_type_error',
     errorClass: 'popup__input_error'
 };
-
+// при вводе текста в input появляется текст ошибки
 const showError = (formElement, inputElement, errorMessage) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
     inputElement.classList.add('popup__input_type_error');
-    errorElement.classList.add('popup__input_error_active');
+    errorElement.classList.add('popup__input_error');
     errorElement.textContent = errorMessage;
 };
-
+// убирает текст ошибки если input валиден
 const hideError = (formElement, inputElement) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
     inputElement.classList.remove('popup__input_type_error');
-    errorElement.classList.remove('popup__input_error_active');
+    errorElement.classList.remove('popup__input_error');
     errorElement.textContent = '';
 };
 // проверка на валидность
 const checkValidity = (formElement, inputElement) => {
     const errorMessage= inputElement.validationMessage;
+
     if(!inputElement.validity.valid) {
         showError(formElement, inputElement, errorMessage);
     } else {
         hideError(formElement, inputElement); 
     };
 };
+// проверка валидации во всех input
+const toggleButtonState = (inputList, buttonElement) => {
+    const hasInvalidInput = Array.from(inputList).some((inputElement) => {
+        return !inputElement.validity.valid;
+    });
 
+    if (hasInvalidInput) {
+        buttonElement.classList.add('popup__button_disable');
+        buttonElement.setAttribute('disable', true);
+    } else {
+        buttonElement.classList.remove('popup__button_disable');
+        buttonElement.removeAttribute('disable');
+    }
+};
+// const hasInvalidInput = (inputList) => {
+//     return Array.from(inputList).some((inputElement) => {
+//         return !inputElement.validity.valid;
+//     })
+// };
+
+// const toggleButtonState = (inputList, buttonElement) => {
+//     if (hasInvalidInput(inputList)) {
+//         buttonElement.classList.add('popup__button_disable');
+//     } else {
+//         buttonElement.classList.remove('popup__button_disable');
+//     }
+// };
 const setEventListener = (formElement) => {
     const inputList = formElement.querySelectorAll('.popup__input');
+    const buttonElement = formElement.querySelector('.popup__button');
+    
     inputList.forEach((inputElement) => {
         inputElement.addEventListener('input', (evt) => {
             checkValidity(formElement, inputElement);
-            console.log(inputElement)
+            toggleButtonState(inputList, buttonElement)
         });
     });
 };
