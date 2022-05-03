b94a76656dda671ba979222be69839c1ae543940
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-05-03 14:45:44 +0300

    ci: сделать класс Popup

--- scripts/components/Popup.js
+++ scripts/components/Popup.js
@@ -1,34 +1,21 @@
 export class Popup {
-    constructor(popupSelector) {
-        this._popupSelector = popupSelector;
-        this._handleEscClose = this._handleEscClose.bind(this);
-        this._handleOverlayClose = this._handleOverlayClose.bind(this)
-        this._closeBtn = this._popupSelector.querySelector('.popup__close-btn')
-    }
-    open() {// публичный метод открытия popup
-        this._popupSelector.classList.add('popup_opened');
-        document.addEventListener('keydown', this._handleEscClose);
-        document.addEventListener('mousedown', this._handleOverlayClose);
-    }
+    
+constructor(popupSelector) {
+    this._popup = popupSelector;
+    this._handleEscClose = this._handleEscClose.bind(this);
+    this._handleOverlayClose = this._handleOverlayClose.bind(this)
+    this._closeBtn = this._popup.querySelector('.popup__close-btn')
+}
+open() {// публичный метод открытия popup
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+    document.addEventListener('mousedown', this._handleOverlayClose);
+}
 
-    close() {// публичный метод закрытия popup
-        this._popupSelector.classList.remove('popup_opened');
-        document.removeEventListener('keydown', this._handleEscClose);
-        document.removeEventListener('mousedown', this._handleOverlayClose);
-    }
+close() {// публичный метод закрытия popup
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+    document.removeEventListener('mousedown', this._handleOverlayClose);
+}
 
-    _handleEscClose(evt) {// приватный метод закрытия popup клавишей ESC
-        if (evt.key === 'Escape') {
-            this.close()
-        };
-    }
-
-    _handleOverlayClose(evt) {
-        if (evt.target.classList.contains('popup_opened')) {
-            this.close();
-        }
-        else if (evt.target === this._closeBtn) {
-            this.close();
-        }
-    }
 }
--- scripts/components/PopupWithForm.js
+++ scripts/components/PopupWithForm.js
@@ -1,32 +1,32 @@
-import { Popup } from "./Popup.js"
+// import { Popup } from "./Popup.js"
 
-export class PopupWithForm extends Popup {
-    constructor(popupSelector, { handleSubmit }) {
-        super(popupSelector);
-        this._handleSubmit = handleSubmit;
-        this._formElement = this._popupSelector.querySelector('.popup__form');
-        this._inputList = this._popupSelector.querySelectorAll('.popup__input');
-    }
+// export class PopupWithForm extends Popup {
+//     constructor(popupSelector, { handleSubmit }) {
+//         super(popupSelector);
+//         this._handleSubmit = handleSubmit;
+//         this._formElement = this._popupSelector.querySelector('.popup__form');
+//         this._inputList = this._popupSelector.querySelectorAll('.popup__input');
+//     }
 
-    _getInputValues() {
-        this._inputValues = {};
-        this._inputList.forEach(input => {
-            this._inputValues[input.name] = input.value;
-        });
-        return this._inputValues;
-    }
+//     _getInputValues() {
+//         this._inputValues = {};
+//         this._inputList.forEach(input => {
+//             this._inputValues[input.name] = input.value;
+//         });
+//         return this._inputValues;
+//     }
 
-    setEventListeners() {
-        this._popupSelector.addEventListener('submit', (evt) => {
-            evt.preventDefault();
-            this._handleSubmit(this._getInputValues());
-            this._close();
-        })
-        super._setEventListeners()
-    }
+//     setEventListeners() {
+//         this._popupSelector.addEventListener('submit', (evt) => {
+//             evt.preventDefault();
+//             this._handleSubmit(this._getInputValues());
+//             this._close();
+//         })
+//         super._setEventListeners()
+//     }
 
-    close() {
-        this._formElement.reset();
-        super.close();
-    }
-};
+//     close() {
+//         this._formElement.reset();
+//         super.close();
+//     }
+// };
