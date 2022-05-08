b41ad6330725fd3724d046242e974d82d104ed53
Vladislav Evdokimov <gangrena14@yandex.ru>
2022-05-08 14:50:28 +0300

    refactor: изменить класс Section

--- src/scripts/components/Section.js
+++ src/scripts/components/Section.js
@@ -1,18 +1,17 @@
 export class Section {
     constructor ({ items, renderer }, selector) {
-        this._items = items; //массив данных
-        this._renderer = renderer;  //отрисовка данных
+        this._items = items;
+        this._renderer = renderer;
         this._container = document.querySelector(selector);
     }
 
-    setItem(element) {
-        const renderedItem = this._renderer(element);
-        this._container.prepend(renderedItem);
+    addItem(element) { 
+        this._container.prepend(element);
     }
     
     renderItems() {
         this._items.forEach((item) => {
-          this.setItem(item);
+          this._renderer(item);
         });
     }
 }
